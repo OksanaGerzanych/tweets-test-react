034283d0a23a51529b03962dedb59b06b5f7cbe3
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-04-25 19:04:06 +0300

    add spiner

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -5,11 +5,15 @@ import { useDispatch } from "react-redux/es/exports";
 import { Link } from 'react-router-dom';
 import { Filter } from "../componens/Filter/Filter";
 import { ButtonBack } from "../componens/Filter/Filter.styled";
+import { selectIsLoading } from "../redux/selectors";
+import { Loader } from "../componens/Loader/Loader";
+import { useSelector } from "react-redux";
 
 
 export default function Tweets() {
   
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const isLoading = useSelector(selectIsLoading);
 
      useEffect(() => {
     dispatch(fetchTweets());
@@ -18,6 +22,7 @@ export default function Tweets() {
     
     return (
         <>
+           { isLoading && <Loader /> }
             <Link to="/"><ButtonBack type="button">Back</ButtonBack></Link>
             <Filter />
             <TweetsList />
